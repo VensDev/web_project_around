c688952a63a258273e0d4ac8fabcd56b9c4dbb88
ViniciusSeixas <vinicius.seixas35@gmail.com>
2024-08-12 22:57:34 +0100

    javascript and test keyframe

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,23 @@
-function show(popup){
-    document.getElementById(popup).style.display = 'block'
+let popUp = document.querySelector(".popup");
+let editProfile = document.querySelector(".profile__edit-button");
+let closeEdit = document.querySelector(".popup__close-button")
+
+
+function appearEditPopUp(){
+    popUp.style.display = "block";
 }
-function hide(popup){
-    document.getElementById(popup).style.display = 'none'
+editProfile.addEventListener("click", appearEditPopUp);
+
+
+function closeProfile(){
+    popUp.style.display = "none";
 }
+closeEdit.addEventListener("click", closeProfile);
+
+
+// document.getElementById('editButton').addEventListener('click', function() {
+//     var formContainer = document.getElementById('formContainer');
+//     formContainer.style.top = '50%';
+//     formContainer.style.transform = 'translate(-50%, -50%)';
+// });
+
