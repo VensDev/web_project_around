0bb4e5eba205ad7af475e12703baad43d2fc9d3a
ViniciusSeixas <vinicius.seixas35@gmail.com>
2024-08-14 13:53:21 +0100

    profile submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,40 @@
+
 let popUp = document.querySelector(".popup__content");
-let editProfile = document.querySelector(".profile__edit-button");
+let editButton = document.querySelector(".profile__edit-button");
 let closeEdit = document.querySelector(".popup__close-button")
 let popUpTemplate = document.querySelector(".popup")
 
+let saveProfile = document.querySelector(".popup__button");
+let nameInput = document.querySelector('#name');
+let job = document.querySelector('#job');
+let profileName = document.querySelector(".profile__name");
+let subJob = document.querySelector(".profile__subtitle");
+
 function appearEditPopUp(){
+
     popUp.style.display = "block";
-    popUpTemplate.style.display = "block"
+    popUpTemplate.style.display = "block";
+
 }
-editProfile.addEventListener("click", appearEditPopUp);
+editButton.addEventListener("click", appearEditPopUp);
 
 
-function closeProfile(){
+function closePopUp(){
+
     popUp.style.display = "none";
-    popUpTemplate.style.display = "none"
-}
-closeEdit.addEventListener("click", closeProfile);
+    popUpTemplate.style.display = "none";
 
+}
+closeEdit.addEventListener("click", closePopUp);
+
+function handleProfileFormSubmit(evt) {
+    evt.preventDefault();  
+
+    profileName.textContent = nameInput.value;
+    subJob.textContent = job.value;
+
+    closePopUp();
+}   
+ saveProfile.addEventListener("click", handleProfileFormSubmit);
 
 
